5c8507cb797bc9cf94769ff735c46bce0bc09877
Big-Belphegor <m18241899495@163.com>
2018-08-08 10:04:32 +0800

    Location First time commit

---
+++ Day02/01.py
@@ -0,0 +1,54 @@
+__author__ = "Alien"
+# 装饰器，普通版(装饰器不传参数模式)
+# import time
+#
+# def timer(func):
+#     def deco():
+#         start_time = time.time()
+#         func()
+#         stop_time = time.time()
+#         print("The func run time is %s" % (stop_time-start_time))
+#     return deco
+#
+# @timer
+# def test1():
+#     time.sleep(2)
+#     print("This is test1")
+#
+# test1()
+
+# 装饰器，升级版(根据默认传入参数运行不同"认证")
+# user1,passwd1 = 'Alien','123456'
+# sshd_key = 'Jsion654321'
+#
+# def auth(auth_type):
+#     def out_wrapper(func):
+#             def wrapper(*args, **kwargs):
+#                 if auth_type == "userpw":
+#                     username = input("Username:")
+#                     password = input("Password:")
+#                     if username == user1 and password == passwd1:
+#                         print("Success in boarding!")
+#                         func(*args,**kwargs)
+#                     else:
+#                         print("Invalid username or password!")
+#                 elif auth_type == "sshdkey":
+#                     keyname = input("Your key:")
+#                     if keyname == sshd_key:
+#                         print("Success in boarding!")
+#                         func(*args, **kwargs)
+#                     else:
+#                         print("Invalid username or password!")
+#             return wrapper
+#     return out_wrapper
+#
+# @auth(auth_type="userpw")
+# def test1():
+#     print("This is test1")
+#
+# @auth(auth_type="sshdkey")
+# def test2():
+#     print("This is test2")
+#
+# test1()
+# test2()
