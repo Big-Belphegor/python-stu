b91d1006c88d0083aab8f81eb12e6e53b7c5132c
Big-Belphegor <m18241899495@163.com>
2018-08-10 15:09:37 +0800

    20180810

--- Day03/01.py
+++ Day03/01.py
@@ -1,20 +1,24 @@
 __author__ = "Alien"
 
-import time
+import time,datetime
 
-print(time.time())                  # 时间戳，以秒为单位
-print(time.localtime())             # 将本地当前时间戳转为时间组
-print(time.gmtime(1820455584))      # 将自定义时间戳转换为时间组
+# print(time.time())                  # 时间戳，以秒为单位
+# print(time.localtime())             # 将本地当前时间戳转为时间组
+# print(time.gmtime(1820455584))      # 将自定义时间戳转换为时间组
+#
+# print(time.timezone)                # 时区
+# a = -28800/3600                     # 中国在东八区(UTC-8)，所以是-8.0
+# print(a)
+#
+# x = time.localtime()                # 获取时间组中某个值
+# print(x)
+# print(x.tm_yday)
+#
+# print(time.mktime(x))               # 将时间组转换为时间戳
+#
+# print(time.strftime("%y-%m-%d %H:%M:%S"))                       # 将当前时间转为字符串
+# print(time.strptime("18-08-10 14:56:30",'%y-%m-%d %H:%M:%S'))   # 将时间字符串转为时间元组
 
-print(time.timezone)                # 时区
-a = -28800/3600                     # 中国在东八区(UTC-8)，所以是-8.0
-print(a)
+# print(datetime.datetime.now())        # 查看当前时间
 
-x = time.localtime()                # 获取时间组中某个值
-print(x)
-print(x.tm_yday)
-
-print(time.mktime(x))               # 将时间组转换为时间戳
-
-print(time.strftime("%y-%m-%d %H:%M:%S"))                       # 将当前时间转为字符串
-print(time.strptime("18-08-10 14:56:30",'%y-%m-%d %H:%M:%S'))   # 将时间字符串转为时间元组
+import random
