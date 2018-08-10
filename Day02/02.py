360dca6d9462cff956015d2b2252fb3e727e6f91
Big-Belphegor <m18241899495@163.com>
2018-08-10 10:56:37 +0800

    20180810 9:39

--- Day02/02.py
+++ Day02/02.py
@@ -1,4 +1,12 @@
 __author__ = "Alien"
+
 import sys,os
-print(sys.path)
+
 print(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append('F:\\python-stu\\Day02\\test')
+print(sys.path)
+
+from testm import *
+a()
+
