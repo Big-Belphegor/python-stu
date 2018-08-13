02162458d82b3ba9e5a69b93adbda20a5954ec34
Big-Belphegor <m18241899495@163.com>
2018-08-13 14:22:01 +0800

    20180813

--- Day03/xml转换/02.py
+++ Day03/xml转换/02.py
@@ -3,15 +3,15 @@ __author__ = "Alien"
 import xml.etree.ElementTree as ET
 
 tree = ET.parse("xmltest.xml")
-root = tree.getroot()
-print(root.tag)
+head = tree.getroot()           # 获取xml的标签头，如本例中的data
+print(head.tag)
 
 # 遍历xml文档
-for child in root:
-    print(child.tag, child.attrib)
-    for i in child:
+for body in head:               # 获取第二层的标签头及标签属性
+    print(body.tag, body.attrib)
+    for i in body:              # 获取第三层的标签头及标签属性
         print(i.tag, i.text)
 
-# 只遍历year 节点
-# for node in root.iter('year'):
-#     print(node.tag, node.text)
+只遍历year节点
+for node in head.iter('year'):  # 获取第二层标签头为‘year’
+    print(node.tag, node.text)
