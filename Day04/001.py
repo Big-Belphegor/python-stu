b7c0f64aab318b3d3d290815a4f4892a4838b9cd
Big-Belphegor <m18241899495@163.com>
2018-08-21 11:12:59 +0800

    20180813

--- Day04/001.py
+++ Day04/001.py
@@ -4,11 +4,11 @@ class test:
     n = 123 # 类变量
     def __init__(self,name,sex,age):
         # 构造函数
-        self.name = name        # 实例变量
+        self.name = name        # 实例变量(实例变量的作用域就是实例本身)
         self.sex = sex
         self.age = age
 
-    def profile(self):
+    def profile(self):          # 类的方法，功能
         print('''
             Your name: %s
             Your sex: %s
@@ -16,6 +16,6 @@ class test:
         ''' % (self.name,self.sex,self.age))
 
 print(test)
-a = test('Alien','M','24')
+a = test('Alien','M','24')      # 实例化
 a.profile()
 print(test.n)
