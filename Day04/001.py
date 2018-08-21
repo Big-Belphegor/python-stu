8d17e77a31782aedd0a88ab742fc8608078fbfff
Big-Belphegor <m18241899495@163.com>
2018-08-21 10:50:12 +0800

    20180813

--- Day04/001.py
+++ Day04/001.py
@@ -1,7 +1,8 @@
 __author__ = "Alien"
 
 class test:
-    def __init__(self,name,sex,age):
+    n = 123 # 类变量
+    def __init__(self,name,sex,age):    # 实例变量
         # 构造函数
         self.name = name
         self.sex = sex
@@ -17,3 +18,4 @@ class test:
 print(test)
 a = test('Alien','M','24')
 a.profile()
+print(test.n)
