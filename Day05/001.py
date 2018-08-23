d633f72db421df2a8a5202bc876f51fdcc05589d
Big-Belphegor <m18241899495@163.com>
2018-08-23 08:15:09 +0800

    20180823

--- Day05/001.py
+++ Day05/001.py
@@ -1,1 +1,19 @@
 __author__ = "Alien"
+
+# 静态方法
+class test(object):
+    def __init__(self,x):
+        self.x = x
+    @staticmethod       # 让其下方的方法不能调用类的self
+    def fun1(self):     # 添加该静态方法后将self去掉
+        # print('%s is %s' % (self.x,'Teacher'))
+        print('%s is %s' % ('Jsion','Teacher'))
+
+# a = test('Jsion')
+# a.fun1()
+
+# b = test('xxx')
+# b.fun1(b)
+
+
+
