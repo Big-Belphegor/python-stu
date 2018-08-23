aba3f181d0459f04f2b4a1032a431c97b1dca87b
Big-Belphegor <m18241899495@163.com>
2018-08-23 16:40:29 +0800

    20180823

--- Day05/002.py
+++ Day05/002.py
@@ -7,18 +7,23 @@ __author__ = "Alien"
 #     --有可能出现问题的代码段
 #     except 错误类型 as e:
 #     --执行遇到该类型错误后执行的内容
+#     else:
+#     --否则就是没有错误
+
+
 
 # 实例：
 # a = [1,2]
 # b = {}
-#
+
 # try:
-#     a[3]
-#     b['haha']
+    # a[3]
+    # b['haha']
+#     a[2]
 #
 # except IndexError as e:
 #     print('Index不存在',e)
-#
+
 # except KeyError as e:
 #     print('Key不存在',e)
 #
@@ -27,4 +32,25 @@ __author__ = "Alien"
 #
 # except Exception as e:                # 抓位置错误
 #     print('出错了，具体是啥不知道',e)
+
+# else:
+#     print('一切正常')
+#
+# finally:                                # 不论是否出错，都会运行
+#     print('谁都不能阻止我运行')
+#
+# 自定义异常
+class DIYError(Exception):
+    def __init__(self,msg):
+        self.message = msg
+
+    def __str__(self):
+        return self.message
+try:
+    raise DIYError('自定义的异常')
+except DIYError as e:
+    print(e)
+
+
+
 
