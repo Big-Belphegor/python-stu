aff836b0746b108e83823b76608371fda8b53840
Big-Belphegor <m18241899495@163.com>
2018-08-23 11:10:08 +0800

    20180823

--- Day05/001.py
+++ Day05/001.py
@@ -31,14 +31,32 @@ __author__ = "Alien"
 # c.fun1()
 
 # 属性方法
-class test3(object):
-    def __init__(self,z):
-        self.z = z
+# class test3(object):
+#     def __init__(self,z):
+#         self.z = z
+#
+#     @property     # 作用：把一个方法变成一个静态属性
+#     def fun1(self):
+#         print('%s and %s' % (self.z,'B'))
+#
+# d = test3('AAA')
+# # d.fun1()        # 加括号会报错
+# d.fun1
+# 注意：此时的d.fun1默认是不能删除的(del d.fun1)
 
-    @property     # 作用：把一个方法变成一个静态属性
+# 反射
+class test4(object):
+    def __init__(self,name):
+        self.name = name
+
     def fun1(self):
-        print('%s and %s' % (self.z,'B'))
+        print('%s is beauty' % self.name)
 
-d = test3('AAA')
-# d.fun1()        # 加括号会报错
-d.fun1
+
+e = test4('Hy')
+e1 = input('>>').strip()
+
+print(hasattr(e,e1))        # hasattr 判断一个对象‘e’中是否有对应字符串的方法映射
+
+fun2 = getattr(e,e1)        # getattr 根据输入的字符串，去对象'e'中查找对应的方法的内存地址
+fun2()                      # 运行匹配到的内存地址
