d18c1eed4b2fa2e0a271ce0037bc73586b444df9
Big-Belphegor <m18241899495@163.com>
2018-08-23 13:38:16 +0800

    20180823

--- Day05/001.py
+++ Day05/001.py
@@ -45,11 +45,7 @@ __author__ = "Alien"
 # 注意：此时的d.fun1默认是不能删除的(del d.fun1)
 
 # 反射
-# 部分一
-# def fun2(*args):
-#     # print('This is fun2：%s' % self.name)      # 也可以是self，直接调用实例参数
-#     print('This is fun2：%s' % args)
-#
+# 部分一(hasattr 和 getattr)
 # class test4(object):
 #     def __init__(self,name):
 #         self.name = name
@@ -59,19 +55,40 @@ __author__ = "Alien"
 #
 #
 # e = test4('Hy')
-# e1 = input('>>').strip()
+# f = input('>>').strip()
 #
-# if hasattr(e,e1):               # hasattr 判断一个对象‘e’中是否有对应'e1'变量中的字符串的方法映射，格式：hasattr(obj,str)
-#     a = getattr(e,e1)           # getattr 根据输入的字符串，去对象'e'中查找对应的方法的内存地址,格式：getattr(obj,str)
-#     a()                         # 运行匹配到的内存地址
+# if hasattr(e,f):               # hasattr 判断一个对象‘e’中是否有对应'e1'变量中的字符串的方法映射，格式：hasattr(obj,str)
+#     a = getattr(e,f)           # getattr 根据输入的字符串，去对象'e'中查找对应的方法的内存地址,格式：getattr(obj,str)
+#     a()                        # 运行匹配到的内存地址
 # else:
-#     setattr(e,e1,fun2)          # 通过字符串的形式，将类外的方法映射到‘类里’,格式：setattr(obj,str,fun)
-#     # fun2(e)                   # 将对象传入
-#     fun2('惊喜不')
+#     print('Not found!')
 
-# 部分二
+# 部分二(setattr使用模式一)
+# class test4(object):
+#     def __init__(self,name):
+#         self.name = name
+#
+#     def fun1(self):
+#         print('%s is beauty' % self.name)
+#
+#
+# e = test4('Hy')
+# f = input('>>').strip()
+#
+# if hasattr(e,f):               # hasattr 判断一个对象‘e’中是否有对应'e1'变量中的字符串的方法映射，格式：hasattr(obj,str)
+#     a = getattr(e,f)           # getattr 根据输入的字符串，去对象'e'中查找对应的方法的内存地址,格式：getattr(obj,str)
+#     a()                        # 运行匹配到的内存地址
+# else:
+#     setattr(e,f,None)          # 如果输入的字符串不存在类中，就自行创建一个函数。格式：setattr(obj,str,fun)
+#     b = getattr(e,f)
+#     print(b)
+
+# 部分三(setattr使用模式二 和 delattr)
 def fun2(self):
     print('This is fun2:',self.name)
+
+def fun3(x):
+    print('This is fun3:',x)
 
 class test5(object):
     def __init__(self,name):
@@ -86,6 +103,9 @@ b = input('>>').strip()
 if hasattr(a,b):
     delattr(a,b)            # 删除一个attr此时再运行程序输入fun1回车会报错(提示找不到这个函数)，格式：delattr(obj,str)
 else:
-    setattr(a,b,fun2)
-    fun2(a)
+    # setattr(a,b,fun2)       # 通过字符串的形式，将类外的方法映射到‘类里’,格式：setattr(obj,str,fun)
+    # fun2(a)
+    setattr(a,b,fun3)       # 注意：此处是重新传进了一个新的函数fun3
+    c = getattr(a,b)        # 此时的c等于附上了函数fun3
+    c(b)
 
