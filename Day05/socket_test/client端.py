48004f4fde07e9d0bece28070da4914eb6db7c89
Big-Belphegor <m18241899495@163.com>
2018-08-24 14:21:20 +0800

    20180823

--- Day05/socket_test/client端.py
+++ Day05/socket_test/client端.py
@@ -1,11 +1,12 @@
 __author__ = "Alien"
 # 客户端
 import socket
-client = socket.socket()            # 声明socket类型，同时生成socket连接对象
 
-client.connect(('localhost',8686))
-client.send(b'Hello world')
-data = client.recv(1024)
-print("recv:",data)
+client = socket.socket()                # 声明socket类型，同时生成socket连接对象
+client.connect(('localhost',8080))      # 连接端口
+
+client.send(b'Hello world')             # 要发送的数据
+data = client.recv(1024)                # 接受数据，大小设置为1024字节
+print("recv:",data)                     # 打印收到的数据
 
 client.close()
--- Day05/socket_test/server端.py
+++ Day05/socket_test/server端.py
@@ -4,16 +4,15 @@ __author__ = "Alien"
 import socket
 server = socket.socket()
 
-server.bind(('localhost',8686))     # 绑定要监听端口
-server.listen()     # 监听端口
-
+server.bind(('localhost',8080))     # 设置监听端口
+server.listen()                     # 开始监听端口
 print('电话连接中...')
-conn,addr = server.accept()     # 等电话
-print(conn,addr)
 
+conn,addr = server.accept()         # 双方端口连接成功
 print('182xxxx88968来电了')
-data = conn.recv(1024)
-print('recv:',data)
-conn.send(data.upper())
+
+data = conn.recv(1024)              # 接受连接数据，设置大小为1024字节
+print('recv:',data)                 # 打印接受到的数据
+conn.send(data.upper())             # 将接受到的数据改为大写，重新发送给出去
 
 server.close()
