2826a18389e1cf200fa1f4e6469ad08ac18607d3
Big-Belphegor <m18241899495@163.com>
2018-08-24 14:44:34 +0800

    20180823

--- Day05/socket_test/client端.py
+++ Day05/socket_test/client端.py
@@ -5,9 +5,10 @@ import socket
 client = socket.socket()                # 声明socket类型，同时生成socket连接对象
 client.connect(('localhost',8080))      # 连接Server端口
 
-client.send('我是Jsion'.encode('utf-8'))
-# client.send(b'Hello world')             # 发送数据给Server，注意：python3后数据必须是二进制
+# client.send('我是Jsion'.encode('utf-8'))    # 可以发送中文，需要转码
+client.send(b'Hello world')             # 发送数据给Server，注意：python3后数据必须是二进制
 data = client.recv(1024)                # 接受数据，大小设置为1024字节
-print("recv:",data.decode())                     # 打印收到的数据
+print("recv:",data)                     # 打印收到的数据
+# print("recv:",data.decode())            # 打印中文，需转码
 
 client.close()
