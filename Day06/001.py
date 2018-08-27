5e55ba3ea0dc48656b951eef2f9dedfdf12ce00e
Big-Belphegor <m18241899495@163.com>
2018-08-27 10:07:32 +0800

    20180823

--- Day06/001.py
+++ Day06/001.py
@@ -1,1 +1,6 @@
 __author__ = "Alien"
+import importlib        # 动态导入模块
+
+a = importlib.import_module('test-module.test-1')       # 直接将test-1模块导入
+print(a)
+print(a.tt().name)
---
+++ Day06/test-module/test-1.py
@@ -0,0 +1,5 @@
+__author__ = "Alien"
+class tt(object):
+    def __init__(self):
+        self.name = 'haha'
+
