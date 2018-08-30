45ae1f597fd73ceccf0f4540599c32cdb8f4f9c3
Big-Belphegor <m18241899495@163.com>
2018-08-30 09:54:22 +0800

    20180830

--- Day07/001.py
+++ Day07/001.py
@@ -1,25 +1,27 @@
 __author__ = "Alien"
 # # SSH-连接
 import paramiko
-# # 实例化ssh客户端
-# ssh = paramiko.SSHClient()
-# # 连接远程节点
-# ssh.connect(hostname='192.168.181.155',port=22,username='test',password='123123')
-# # 执行命令
-# stdin,stdout,stderr = ssh.exec_command('df')    # stdin:标准输入；stdout:标准输出;stderr:标准错误
-# # 获取命令结果
-# result = stdout.read()
-# # 关闭连接
-# ssh.close()
+# 实例化ssh客户端
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# 连接远程节点
+ssh.connect(hostname='192.168.181.155',port=22,username='test',password='123123')
+# 执行命令
+stdin,stdout,stderr = ssh.exec_command('df')    # stdin:标准输入；stdout:标准输出;stderr:标准错误
+# 获取命令结果
+result = stdout.read()
+print(result.decode())
+# 关闭连接
+ssh.close()
 
 # SSH-SCP
-transport = paramiko.Transport(('192.168.181.155',22))      # 与服务器建立连接
-transport.connect(username='test',password='123123')        # 连接传输用户
-
-sftp = paramiko.SFTPClient.from_transport(transport)        # 建立文件传输通道
-# 上传文件，格式：xxx.put('本地文件路径+文件名' ‘远程文件路径’)
-sftp.put('C:\\Users\\dxw-user\\Desktop\\01.png','/home/test/01.png')
-# 下载文件，格式：xxx.get('远程文件路径' ‘本地保存路径+文件名’)
-sftp.get('/home/test/tt','C:\\Users\\dxw-user\\Desktop\\tt_new')
-transport.close()
+# transport = paramiko.Transport(('192.168.181.155',22))      # 与服务器建立连接
+# transport.connect(username='test',password='123123')        # 连接传输用户
+#
+# sftp = paramiko.SFTPClient.from_transport(transport)        # 建立文件传输通道
+# # 上传文件，格式：xxx.put('本地文件路径+文件名' ‘远程文件路径’)
+# sftp.put('C:\\Users\\dxw-user\\Desktop\\01.png','/home/test/01.png')
+# # 下载文件，格式：xxx.get('远程文件路径' ‘本地保存路径+文件名’)
+# sftp.get('/home/test/tt','C:\\Users\\dxw-user\\Desktop\\tt_new')
+# transport.close()
 
