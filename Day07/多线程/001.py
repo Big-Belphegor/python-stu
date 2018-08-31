d68e73e674c16904c2fbedbb294e5de4988cb7f9
Big-Belphegor <m18241899495@163.com>
2018-08-31 10:23:46 +0800

    20180831

--- Day07/多线程/001.py
+++ Day07/多线程/001.py
@@ -17,16 +17,36 @@ import threading,time
 
 
 # 用类实现多线程（有些麻烦，可以使用上面的写法）
-class MyThread(threading.Thread):
-    def __init__(self,n):
-        super(MyThread,self).__init__()
-        self.n = n
+# class MyThread(threading.Thread):
+#     def __init__(self,n):
+#         super(MyThread,self).__init__()
+#         self.n = n
+#
+#     def run(self):
+#         print('running task',self.n)
+#
+# t1 = MyThread('t1')
+# t2 = MyThread('t2')
+#
+# print(t1.run())
+# print(t2.run())
 
-    def run(self):
-        print('running task',self.n)
+# 测试多线程性能,测试50个线程需要多久运行完
+start_time = time.time()
+t_obj = []
+def run(n):
+    print(n)
+    time.sleep(2)
 
-t1 = MyThread('t1')
-t2 = MyThread('t2')
+for i in range(50):
+    t = threading.Thread(target=run,args=('t-%s' % i,))
+    t.start()
+    t_obj.append(t)
+print('当前活动线程个数:%s' % threading.active_count())
 
-print(t1.run())
-print(t2.run())
+for x in t_obj:
+    x.join()            # 功能：让主线程等待子线程执行完毕后再运行
+
+#注意：程序本身就会占用一个线程，这个线程称之为主线程。所以本实例，其实是并行这运行了51个线程。
+print('程序共费时:%s 当前活动线程个数:%s' % (time.time()-start_time,threading.active_count()))
+
