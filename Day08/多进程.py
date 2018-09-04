0357c8f82f8b13a9bffe1328dc41e522945db9a6
Big-Belphegor <m18241899495@163.com>
2018-09-04 13:15:52 +0800

    20180904

--- Day08/多进程.py
+++ Day08/多进程.py
@@ -1,17 +1,37 @@
 __author__ = "Alien"
-# 实现多进程，并在进程中添加线程
-import multiprocessing,time,threading
+# 创建一个进程，并在进程中添加线程
+import multiprocessing,time,threading,os
 
-def thread_run():
-    print(threading.get_ident())
+# def thread_run():
+#     print(threading.get_ident())
+#
+# def run(name):
+#     time.sleep(2)
+#     print("Hello",name)
+#     t = threading.Thread(target=thread_run)     # 开启一个进程
+#     t.start()                                   # 运行这个进程
+#
+# if __name__=='__main__':
+#     for i in range(10):
+#         p = multiprocessing.Process(target=run,args=('Jsion',))
+#         p.start()
 
-def run(name):
-    time.sleep(2)
-    print("Hello",name)
-    t = threading.Thread(target=thread_run)     # 开启一个进程
-    t.start()                                   # 运行这个进程
+# 通过父进程创建子进程
+def info(n):
+    #父进程
+    print(n)
+    print('module name:',__name__)
+    print('parent process:',os.getppid())       # 获取父进程pid
+    print('process id:',os.getpid())            # 获取进程本身pid
+    print('\n\n')
 
-if __name__=='__main__':
-    for i in range(10):
-        p = multiprocessing.Process(target=run,args=('Jsion',))
-        p.start()
+def f(n):
+    #子进程
+    info('\033[31;1mfunction f\033[0m')
+    print('Hello',n)
+
+if __name__ == '__main__':
+    info('\033[32;1mmain process line\033[0m')
+    p = multiprocessing.Process(target=f,args=('Jsion',))
+    p.start()
+    p.join()
