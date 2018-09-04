22152fae1b891b5b5ba10cfd23e03782e67f466a
Big-Belphegor <m18241899495@163.com>
2018-09-04 11:36:52 +0800

    20180904

--- Day08/多进程.py
+++ Day08/多进程.py
@@ -8,8 +8,8 @@ def thread_run():
 def run(name):
     time.sleep(2)
     print("Hello",name)
-    t = threading.Thread(target=thread_run)
-    t.start()
+    t = threading.Thread(target=thread_run)     # 开启一个进程
+    t.start()                                   # 运行这个进程
 
 if __name__=='__main__':
     for i in range(10):
