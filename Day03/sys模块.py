cf43f12c5b82de1c43e4f896170293e919554399
Big-Belphegor <m18241899495@163.com>
2018-09-25 10:42:40 +0800

    20180925

--- Day03/sys模块.py
+++ Day03/sys模块.py
@@ -1,1 +1,10 @@
 __author__ = "Alien"
+import sys
+print(sys.argv)     # 获取当前模块
+
+# print(sys.exit())   # 退出程序
+print(sys.path)     # 返回模块的搜索路径
+print(sys.platform) # 返回系统名称
+sys.stdout.write('please:')
+val = sys.stdin.readline()[:-1]
+print(val)
