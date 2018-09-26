e9372a394354162c4c022cac0977e816beee1686
Big-Belphegor <m18241899495@163.com>
2018-09-26 11:53:32 +0800

    20180926

---
+++ Day03/logging模块.py
@@ -0,0 +1,35 @@
+__author__ = "Alien"
+import logging,time
+# 日志级别
+# 日志级别等级CRITICAL > ERROR > WARNING > INFO > DEBUG）
+# 默认只输出大于warning级别日志内容
+# logging.debug('debug message')
+# logging.info('info message')
+# logging.warning('warning message')
+# logging.error('error message')
+# logging.critical('critical message')
+
+# 通过basicConfig定义日志输出级别、格式等，具体参数访问有道云笔记--Python--02、模块及内置方法
+# logging.basicConfig(level=logging.INFO,                             # 大于INFO级别的都输出
+#                     format="%(asctime)s %(name)s %(levelname)s %(message)s",
+#                     datefmt = '%Y-%m-%d  %H:%M:%S %a'               # 注意月份和天数不要搞乱了，这里的格式化符与time模块相同
+#                     )
+#
+# logging.debug('debug message')
+# logging.info('info message')
+# logging.warning('warning message')
+# logging.error('error message')
+# logging.critical('critical message')
+
+# 将日志写入到文件
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+                    datefmt='%Y-%m-%d  %H:%M:%S %a',
+                    filename='t1.log',
+                    filemode='w',
+                    )
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
