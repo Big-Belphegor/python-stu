92bf5979a190a5172531c2173194c8e0ec359089
Big-Belphegor <m18241899495@163.com>
2019-01-28 10:40:44 +0800

    20181108

--- Day11/001.py
+++
@@ -1,5 +0,0 @@
-__author__ = "Alien"
-a = 'Python '
-
-print(a+'< end')
-print(a.rstrip()+'< end')       # 删除尾部空字符
---
+++ Day11/复习.py
@@ -0,0 +1,33 @@
+__author__ = "Alien"
+# a = 'Python '
+# b = ' Python'
+#
+# print(a+'< end')
+# print(a.rstrip()+'< end')       # 删除尾部空字符
+#
+# print('First >'+b)
+# print('First >'+b.lstrip())     # 删除首部空字符
+#
+# c = ['alient','tom']
+# c.append('end')
+# c.insert(1,'first')
+#
+# d = ['a','b','c']
+# print(d.pop(1))                 # 默认删除列表末尾的值，可以指定索引删除，删除信息可以被再次调用
+#
+# del c[1]
+# c.remove('tom')
+#
+# print(c)
+# print(c[0].title())             # 首字母大写
+#
+# e = ['v','c','a','b','f']
+# e.sort()                        # 永久排序，正向
+# # e.sort(reverse=True)            # 永久排序，反向排序
+# print(e)
+#
+# f = ['c','e','d','a']
+# print(sorted(f))                # 临时排序
+
+num = list(range(1,6))
+print(num)
