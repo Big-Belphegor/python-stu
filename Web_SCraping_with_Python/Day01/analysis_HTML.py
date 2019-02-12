083533887a5560f092a17d5f707096bcc993fa3d
Big-Belphegor <m18241899495@163.com>
2019-02-12 15:31:42 +0800

    20190212

--- Web_SCraping_with_Python/Day01/analysis_HTML.py
+++ Web_SCraping_with_Python/Day01/analysis_HTML.py
@@ -12,13 +12,30 @@ from bs4 import BeautifulSoup
 # print('"The prince" NUM: ',len(testList))
 
 # 实例二，查找子标签及后代标签
+# html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+# bs0bj = BeautifulSoup(html)
+#
+# for child in bs0bj.find('table',{"id":"giftList"}).children:        # 只查找子标签
+#     print(child)
+#
+# print("\n============================\n")
+#
+# for child in bs0bj.find('table',{"id":"giftList"}).descendants:     # 查找后代标签
+#     print(child)
+
+# 实例三，处理兄弟标签
+# html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+# bs0bj = BeautifulSoup(html)
+#
+# for sibling in bs0bj.find("table",{"id":"giftList"}).tr.next_siblings:     # next_siblings作用：跳过第一个匹配的标签
+#     print(sibling)
+# 注意：previous_siblings定位最后一行但不需抓取的标签，next_sibling和previous_sibling为匹配单个上面两个为多个。
+
+# 实例四，处理父标签
 html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs0bj = BeautifulSoup(html)
+print(bs0bj.find("img",{"src":"../img/gifts/img6.jpg"})\
+      .parent.previous_sibling.get_text())                          # parent/parents是抓取父标签函数
 
-for child in bs0bj.find('table',{"id":"giftList"}).children:        # 只查找子标签
-    print(child)
-
-print("\n============================\n")
-
-for child in bs0bj.find('table',{"id":"giftList"}).descendants:     # 查找后代标签
-    print(child)
+print(bs0bj.find("span",{"class":"excitingNote"},text="Keep your friends guessing!")\
+      .parent.next_sibling.get_text())
