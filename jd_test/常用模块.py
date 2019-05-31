6e72b6ea68c54d49692853358bb0a941944cb60e
User1 <1565001921@qq.com>
2019-05-31 16:53:33 +0800

    20190531

--- jd_test/常用模块.py
+++ jd_test/常用模块.py
@@ -171,16 +171,48 @@ import re
 # print(s.replace('ll','xx'))     # 替换字符
 # print(s.split('w'))             # 指定分隔符
 
-# 正式开始介绍re模块
-# 格式：re.方法('匹配规则','被匹配的字符串')
-x = 'Hello world wxxl wxxxl'
+# # 正式开始介绍re模块
+# # 格式：re.方法('匹配规则','被匹配的字符串')
+# x = 'Hello world wxxl wxxxl'
+# y = '18241877987'
+# print(re.findall('H.l',x))          # '.'匹配任意单个字符，除换行符
+# print(re.findall('^H...o',x))       # '^'匹配字符串首字符/字符串
+# print(re.findall('w...l$',x))       # '$'匹配字符串尾字符/字符串
+# print(re.findall('wx*',x))          # '*'匹配时其前面字符/字符串至少匹配0到n次
+# print(re.findall('x+l',x))          # '+'匹配时其前面字符/字符串至少匹配1到n次
+# print(re.findall('x?l',x))          # '?'匹配时其前面字符/字符串至少匹配0到1次
+#
+# print(re.findall('[0-9]{11}',y))    # '{}'匹配其前面字符/字符串出现'{}'内设定的次数，或次数范围
+# print(re.findall('x{1,3}l',x))      # '{}'的次数范围示例
+#
+# print(re.findall('[r,x]l',x))            # '[]'匹配其内部设定的条件，符合便进行匹配
+# print(re.findall('[^a,c]','abcefg'))     # '[^]'表示取反，注意该实例是取字符串内除了a和c的值
+# print(re.findall('[a-z]','abc'))         # '[-]'表示取值范围，可以是0~9,A~Z等
+#
+# print(re.findall('\d','12345789'))                  # '\d'匹配所有数字字符，等同于[0-9]
+# print(re.findall('\w','abc efg123'))                # '\w'匹配所有字符，等同于[0-9a-zA-Z]
+# print(re.findall(r'I\b','Hello,I am Alien'))        # '\b'匹配其前面字符与其后字符中间的特殊字符(如：空格、$等)，注意使用时需要加'r'
+# print(re.findall(r'\bam','Hello,I am Alien'))       # 此处又举例了一个特殊字符在匹配字符前的情况
+# '''
+# 正则表达式中其它特殊元字符：
+# \d      # 匹配任何十进制数，等同于[0-9]
+# \D      # 匹配任何非十进制数的字符，等同于[^0-9]
+# \s      # 匹配任何空白字符，等同于[\t\n\r\f\v]
+# \S      # 匹配任何空非白字符，等同于[^\t\n\r\f\v]
+# \w      # 匹配任何字母数字字符，等同于[0-9a-zA-Z]
+# \W      # 匹配任何非字母数字字符，等同于[^0-9a-zA-Z]
+# \b      # 匹配一个单词的边界，也就是单词和空格的位置
+# '''
 
-print(re.findall('H.l',x))      # '.'匹配任意单个字符，除换行符
-print(re.findall('^H...o',x))   # '^'匹配字符串首字符/字符串
-print(re.findall('w...l$',x))   # '$'匹配字符串尾字符/字符串
-print(re.findall('wx*',x))      # '*'匹配其前面字符/字符串0到n次
-print(re.findall('x+l',x))      # '+'匹配其前面字符/字符串1到n次
-print(re.findall('x?l',x))      # '?'匹配其前面字符/字符串0到1次
+# print(re.findall(r'\\e','abcd\e'))          # '\'转义符，将特殊元字符变为普通字符，也可以将部分普通字符变为特殊元字符，注意匹配其自身时需要加'r'
+# print(re.findall('\\\\e','abcd\e'))         # '\'的另外一种匹配方式
+#
+# print(re.findall('(com|cn)','baidu.com'))   # '|'匹配两个中的一个，表示或的意思
+# print(re.findall('(\+86)','+86abcde'))      # '()'表示一个元素组，将多个字符变成一个整体去进行匹配
 
+
+# re模块的方法
+a = 'abcabcee\e'
+print(re.search('abc',a).group())           # 'search'方法只匹配第一个符合的字符/字符串，注意，search打印需要调用.group()方法
 
 
